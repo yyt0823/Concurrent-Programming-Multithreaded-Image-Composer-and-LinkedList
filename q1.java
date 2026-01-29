1eb9033ac30aa9ae618b348ed17407c43c7f6914
YYT <yantianyin1999@gmail.com>
2026-01-29 18:45:58 -0500

    init

---
+++ q1.java
@@ -0,0 +1,86 @@
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
+
+public class q1 {
+    
+
+    // parameters and their default values
+    public static String imagebase = "icon" ; // base name of the input images; actual names append "1.png", "2.png" etc.
+    public static int threads = 1; // number of threads to use
+    public static int outputheight = 2048; // output image height
+    public static int outputwidth = 2048; // output image width
+    public static int n = 100; // icons added/changed
+    public static final int ICONS = 8; // total number of icons
+
+    // icons image
+    public static BufferedImage[] icons = new BufferedImage[ICONS];
+
+    // output image
+    public static BufferedImage imgout;
+
+    // print out command-line parameter help and exit
+    public static void help(String s) {
+        System.out.println("Could not parse argument \""+s+"\".  Please use only the following arguments:");
+        System.out.println(" -w outputimagewidth (integer; current=\""+outputwidth+"\")");
+        System.out.println(" -h outputimageheight (integer; current=\""+outputheight+"\")");
+        System.out.println(" -t threads (integer value >=1; current=\""+threads+"\")");
+        System.out.println(" -n changes (integer value >=1; current=\""+n+"\")");
+        System.exit(1);
+    }
+
+    // process command-line options
+    public static void opts(String[] args) {
+        int i = 0;
+
+        try {
+            for (;i<args.length;i++) {
+                if (i==args.length-1)
+                    help(args[i]);
+                if (args[i].equals("-h")) {
+                    outputheight = Integer.parseInt(args[i+1]);
+                } else if (args[i].equals("-w")) {
+                    outputwidth = Integer.parseInt(args[i+1]);
+                } else if (args[i].equals("-t")) {
+                    threads = Integer.parseInt(args[i+1]);
+                } else if (args[i].equals("-n")) {
+                    n = Integer.parseInt(args[i+1]);
+                } else {
+                    help(args[i]);
+                }
+                // an extra increment since our options consist of 2 pieces
+                i++;
+            }
+        } catch (Exception e) {
+            System.err.println(e);
+            help(args[i]);
+        }
+    }
+
+    // main.  we allow an IOException in case the image loading/storing fails.
+    public static void main(String[] args) throws IOException {
+        // process options
+        opts(args);
+
+        // read in one of the input images (icons).  You'll need to expand/duplicate this code to read
+        // different icons and find their sizes.
+        for (int i=1;i<=ICONS;i++) {
+            icons[i-1] = ImageIO.read(new File(imagebase+i+".png"));
+        }
+
+        // create an output image
+        BufferedImage outputimage = new BufferedImage(outputwidth,outputheight,BufferedImage.TYPE_INT_ARGB);
+
+        // demonstrate copying an icon into the output image.
+        // this is not the only way to read or modify image pixels, but it is a starting point
+        for (int i=0;i<icons[0].getWidth();i++) {
+            for (int j=0;j<icons[0].getHeight();j++) {
+                outputimage.setRGB(i,j,icons[0].getRGB(i,j));
+            }
+        }
+
+        // Write out the image
+        File outputfile = new File("outputimage.png");
+        ImageIO.write(outputimage, "png", outputfile);
+    }
+}
