10f982da491ed22a8063e25ce3ae2dbbd9c78b7d
YYT <yantianyin1999@gmail.com>
2026-02-01 02:26:48 -0500

    this is probably wrong

--- q1.java
+++ q1.java
@@ -9,16 +9,44 @@ public class q1 {
 
     // parameters and their default values
     public static String imagebase = "icon" ; // base name of the input images; actual names append "1.png", "2.png" etc.
-    public static int threads = 1; // number of threads to use
+    public static int threads = 8; // number of threads to use
     public static int outputheight = 2048; // output image height
     public static int outputwidth = 2048; // output image width
     public static int n = 100; // icons added/changed
     public static final int ICONS = 8; // total number of icons
 
-    // locks 
-    static final Object IMG_LOCK = new Object();
+    // locks not working deprecated
+    // static final Object IMG_LOCK = new Object();
     static final Object COUNT_LOCK = new Object();
     
+    // tile lock
+    static Object[] tilelocks;
+    static final int tileCols = 16;
+    static final int tileRows = 16;
+    static int tileW, tileH;
+
+    static void initTileLocks() {
+        tileW = (outputwidth  + tileCols - 1) / tileCols;   
+        tileH = (outputheight + tileRows - 1) / tileRows;
+    
+        tilelocks = new Object[tileCols * tileRows];
+        for (int i = 0; i < tilelocks.length; i++) tilelocks[i] = new Object();
+    }
+    // deprecated
+    // static int singleTileIndexOrMinus1(int x, int y, int w, int h) {
+    //     if (x < 0 || y < 0 || x + w > outputwidth || y + h > outputheight) return -1;
+    
+    //     int tx0 = Math.min((x) / tileW, tileCols - 1);
+    //     int ty0 = Math.min((y) / tileH, tileRows - 1);
+    //     int tx1 = Math.min((x + w - 1) / tileW, tileCols - 1);
+    //     int ty1 = Math.min((y + h - 1) / tileH, tileRows - 1);
+    
+    //     if (tx0 != tx1 || ty0 != ty1) return -1; // crosses tile boundary
+    //     return ty0 * tileCols + tx0;
+    // }
+
+
+
 
 
     // icons image
@@ -88,6 +116,10 @@ public class q1 {
         // Write out the image
         File outputfile = new File("outputimage.png");
         
+        // init tile locks
+        initTileLocks();
+
+
         // set timer
         long startTime = System.currentTimeMillis();
         //test
@@ -101,15 +133,17 @@ public class q1 {
         for (int i = 0; i < threads; i++) {
             ts[i].join();
         }
+        System.out.println(System.currentTimeMillis() - startTime);
 
         ImageIO.write(imgout, "png", outputfile);
         
-        System.out.println(System.currentTimeMillis() - startTime);
+        
 
     }
 
 
 
+    
 
     // idea: we are tring to access the same outputimage thus this should be our shared memory object
     // for each thread:
@@ -129,72 +163,81 @@ public class q1 {
         public void run() {
             // here we use the thread local random instead of the Random class
             ThreadLocalRandom random = ThreadLocalRandom.current();
-            // pick a random icon
             while (true) {
-                int icon = random.nextInt(ICONS);
-
-                // get the width and height of the icon
-                int width = icons[icon].getWidth();
-                int height = icons[icon].getHeight();
-
-
-                // pick a random position in side of the boundary
-                int x = random.nextInt(outputwidth - width + 1);
-                int y = random.nextInt(outputheight - height + 1);
-
-
-
-
-                synchronized (IMG_LOCK) {
-                // varify the icon
-                    boolean posvalid = true;
-
-                    // check only the boundary of the icon area
-                    for (int i = 0; i < width && posvalid; i++) {
-                        if (imgout.getRGB(x + i, y) != 0) {
-                            posvalid = false;
-                            break;
-                        }
-                        if (imgout.getRGB(x + i, y + height - 1) != 0) {
-                            posvalid = false;
-                            break;
-                        }
-                    }
-                    for (int j = 1; j < height - 1 && posvalid; j++) {
-                        if (imgout.getRGB(x, y + j) != 0) {
-                            posvalid = false;
-                            break;
-                        }
-                        if (imgout.getRGB(x + width - 1, y + j) != 0) {
-                            posvalid = false;
-                            break;
-                        }
-                    }
-
-                    if (!posvalid) {
-                        continue;
-                    }
-
-                    // add the icon to the output image
-                    for (int i = 0; i < width; i++) {
-                        for (int j = 0; j < height; j++) {
-                            int iconPixel = icons[icon].getRGB(i, j);
-                            if (iconPixel != 0) {
-                                imgout.setRGB(x + i, y + j, iconPixel);
-                            }
-                        }
-                    }
-                }
-
+                // decrement the count
                 synchronized (COUNT_LOCK) {
                     if (n <= 0) return;
                     n--;
-                    if (n <= 0) return;
+                }
+                while (true) {
+                    // pick a random icon
+                    int icon = random.nextInt(ICONS);
+                    // get the width and height of the icon
+                    int width = icons[icon].getWidth();
+                    // get the height of the icon
+                    int height = icons[icon].getHeight();
+
+                    // pick a random tile
+                    int tx = random.nextInt(tileCols);
+                    int ty = random.nextInt(tileRows);
+
+                    // get the left and top of the tile
+                    int left = tx * tileW;
+                    int top  = ty * tileH;
+
+                    // get the maximum x and y of the icon
+                    int maxX = Math.min(left + tileW - width,  outputwidth - width);
+                    int maxY = Math.min(top  + tileH - height, outputheight - height);
+
+                    // if the icon can't fit in this tile, continue
+                    if (maxX < left || maxY < top) continue; 
+
+                    int x = left + random.nextInt(maxX - left + 1);
+                    int y = top  + random.nextInt(maxY - top  + 1);
+
+                    int idx = ty * tileCols + tx; // guaranteed single tile
+                    if (idx == -1) {
+                        System.err.println("idx is -1");
+                        continue;
+                    }
+
+
+                    synchronized (tilelocks[idx]) {
+                        // System.err.println(idx);
+                        // // varify the icons
+                        boolean posvalid = true;
+                        // top & bottom edges
+                        for (int i = 0; i < width && posvalid; i++) {
+                            if (imgout.getRGB(x + i, y) != 0) posvalid = false;
+                            if (imgout.getRGB(x + i, y + height - 1) != 0) posvalid = false;
+                        }
+
+                        // left & right edges (skip corners)
+                        for (int j = 1; j < height - 1 && posvalid; j++) {
+                            if (imgout.getRGB(x, y + j) != 0) posvalid = false;
+                            if (imgout.getRGB(x + width - 1, y + j) != 0) posvalid = false;
+                        }
+                        if (!posvalid) {
+                            continue;
+                        }
+    
+                        // add the icon to the output image
+                        for (int i = 0; i < width; i++) {
+                            for (int j = 0; j < height; j++) {
+                              int iconPixel = icons[icon].getRGB(i, j); 
+                              imgout.setRGB(x + i, y + j, iconPixel);   
+                            }
+                          }
+                        }
+                        break;
+                    }
                 }
             }
-
         }
     }
 
 
-}
+                
+            
+        
+
